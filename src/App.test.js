c87141ef07af5ec3b9db1a026fb2b8fdf21981bc
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-10-31 16:59:32 +0000

    Add testing for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -26,22 +26,49 @@ describe("<App> ", () => {
     });
 
     it("should check the correct components are rendered", () => {
-        const apollo = renderedOutput.findAllByType(Apollo);
-        const appContext = renderedOutput.findAllByType(AppContext);
-        const appWrapper = renderedOutput.findAllByType(AppWrapper);
-        const browserRouter = renderedOutput.findAllByType(BrowserRouter);
-        const globalStyle = renderedOutput.findAllByType(GlobalStyle);
-        const header = renderedOutput.findAllByType(Header);
-        const pageWrapper = renderedOutput.findAllByType(PageWrapper);
-        const switches = renderedOutput.findAllByType(Switch);
+        const apollo = renderedOutput.findByType(Apollo);
+        const appContext = apollo.findByType(AppContext);
+        const appWrapper = appContext.findByType(AppWrapper);
+        const browserRouter = appWrapper.findByType(BrowserRouter);
+        const globalStyle = browserRouter.findByType(GlobalStyle);
+        const header = browserRouter.findByType(Header);
+        const pageWrapper = browserRouter.findByType(PageWrapper);
+        const switches = pageWrapper.findByType(Switch);
+        const routes = switches.findAllByType(Route);
+        console.log(routes.length);
 
-        expect(apollo).toHaveLength(1);
-        expect(appContext).toHaveLength(1);
-        expect(appWrapper).toHaveLength(1);
-        expect(browserRouter).toHaveLength(1);
-        expect(globalStyle).toHaveLength(1);
-        expect(header).toHaveLength(1);
-        expect(pageWrapper).toHaveLength(1);
-        expect(switches).toHaveLength(1);
+        // QUESTION - because of switch only one route renders - how do we test others?
+
+        //first route
+        expect(routes[0].props.path).toBe("/");
+        expect(routes[0].props.exact).toBe(true);
+        const route1Content = routes[0].findByType(Content);
+        route1Content.findByType(LandingPage);
+
+        // //second route
+        // expect(routes[1].props.path).toBe("/search");
+        // const route2Content = routes[1].findByType(Content);
+        // route2Content.findByType(SearchPage);
+
+        // //third route
+        // expect(routes[2].props.path).toBe("/my-searches");
+        // const route3Content = routes[2].findByType(Content);
+        // route3div = route3Content.findByType("div");
+        // console.log(route3div.children[0]);
+
+        // //fourth route
+        // expect(routes[3].props.path).toBe("/browse");
+        // const route4Content = routes[3].findByType(Content);
+        // route4div = route4Content.findByType("div");
+
+        // //fifth route
+        // expect(routes[4].props.path).toBe("/about");
+        // const route5Content = routes[4].findByType(Content);
+        // route5Content.findByType(AboutPage);
+
+        // //sixth route
+        // expect(routes[5].props.path).toBe("/help");
+        // const route6Content = routes[5].findByType(Content);
+        // route6div = route6Content.findByType("div");
     });
 });
