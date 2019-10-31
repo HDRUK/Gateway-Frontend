a3bd6d8301d2ac1a5869b432010336d7266dbeaf
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-10-31 11:19:09 +0000

    Update mock

--- src/__mocks__/AppContextMock.js
+++ src/__mocks__/AppContextMock.js
@@ -2,7 +2,17 @@ const context = {
     newsItems: {
         newsItemOne: {
             image: "",
-            description: "Test",
+            description: "HDR release news of new partners in exciting new digital project",
+            readMore: "http://localhost:3000"
+        },
+        newsItemTwo: {
+            image: "",
+            description: "HDR release news of new partners in exciting new digital project",
+            readMore: "http://localhost:3000"
+        },
+        newsItemThree: {
+            image: "",
+            description: "HDR release news of new partners in exciting new digital project",
             readMore: "http://localhost:3000"
         }
     },
