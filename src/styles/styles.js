cda1913478ee1550294eddc48d95f9a1615171d4
Tim Howard <tim.howard@ibm.com>
2019-10-31 15:04:55 +0000

    Write footer tests

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -190,6 +190,35 @@ const NewsTileItem = styled.div`
     }
 `;
 
+const FooterWrapper = styled.div`
+    height: 12rem;
+    padding-bottom: 1rem;
+    background-color: rgb(60, 60, 59);
+    color: white;
+    padding-left: 7rem;
+    font-size: 1.1rem;
+    position: relative;
+`;
+
+const FooterBlock = styled.div`
+    display: inline-block;
+    box-sizing: border-box;
+    width: 33.33%;
+    height: 90%;
+    vertical-align: top;
+    padding: 3rem 5rem 0 0;
+`;
+
+const FooterText = styled.div`
+    width: 100%;
+    margin: 0 0 0.4rem 0;
+`;
+
+const FooterImage = styled(Logo)`
+    max-width: 8rem;
+    max-height: 5rem;
+`;
+
 export {
     Heading,
     ParagraphHeading,
@@ -222,5 +251,9 @@ export {
     ImageBlockWrapper,
     SmallImage,
     NewsTileItem,
-    StyledImage
+    StyledImage,
+    FooterWrapper,
+    FooterBlock,
+    FooterText,
+    FooterImage
 };
