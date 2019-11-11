89f6768b8930ded974b0876805a68cca57b28fb4
Tim Howard <tim.howard@ibm.com>
2019-11-11 17:36:16 +0000

    Pagination work, working when scrolled to bottom

--- src/Containers/SearchPage/SearchPage.js
+++ src/Containers/SearchPage/SearchPage.js
@@ -24,39 +24,101 @@ const text = {
     searchTitle: "What health data to you need?"
 };
 
+const handleScroll = ({ currentTarget }, onLoadMore, offSet, setOffSet, dataLength, loading) => {
+    if (currentTarget.scrollTop + currentTarget.clientHeight >= currentTarget.scrollHeight) {
+        if (!loading && offSet <= dataLength) {
+            onLoadMore(offSet);
+            setOffSet(offSet + 10);
+        }
+    }
+};
+
+const DataFeed = props => {
+    return (
+        <ResultsWrapper
+            onScroll={e =>
+                handleScroll(e, props.onLoadMore, props.offSet, props.setOffSet, props.dataLength, props.loading)
+            }
+        >
+            {props.data.length > 0 ? (
+                props.data.map((result, i) => (
+                    <ResultCard key={`resultCard-${i}`} title={result.label} description={result.description} />
+                ))
+            ) : (
+                <div>No results</div>
+            )}
+            {props.loading && (
+                <CenterLoading active={true} withOverlay={false} description="Active loading indicator" />
+            )}
+        </ResultsWrapper>
+    );
+};
+
+const ResultsData = props => {
+    const { error, loading, data, fetchMore } = useQuery(CATALOGUE_ITEMS_SEARCH, {
+        variables: { recordLimit: 10, recordOffset: 0, searchTerm: props.searchTerm },
+        fetchPolicy: "cache-and-network",
+        notifyOnNetworkStatusChange: true
+    });
+
+    console.log("ResultsData", loading, data && data.hdrCatalogueItemsSearch.data);
+    if (loading && !data)
+        return <CenterLoading active={true} withOverlay={false} description="Active loading indicator" />;
+    if (error) return <div>{error.status} - Error :(</div>;
+
+    props.setDataLength(data.hdrCatalogueItemsSearch.count);
+
+    const processedData = data && data.hdrCatalogueItemsSearch && data.hdrCatalogueItemsSearch.data;
+    return (
+        <DataFeed
+            offSet={props.offSet}
+            setOffSet={props.setOffSet}
+            data={processedData || []}
+            dataLength={props.dataLength}
+            loading={loading}
+            onLoadMore={offSet =>
+                fetchMore({
+                    variables: { recordLimit: 10, recordOffset: offSet, searchTerm: props.searchTerm },
+                    updateQuery: (prev, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev;
+                        return Object.assign({}, prev, {
+                            hdrCatalogueItemsSearch: {
+                                ...prev.hdrCatalogueItemsSearch,
+                                data: [
+                                    ...prev.hdrCatalogueItemsSearch.data,
+                                    ...fetchMoreResult.hdrCatalogueItemsSearch.data
+                                ]
+                            }
+                        });
+                    }
+                })
+            }
+        />
+    );
+};
+
 const SearchPage = () => {
     const appContext = useContext(AppContext);
     const pageState = appContext.state.searchPageState;
     const returnSearchResults = appContext.returnSearchResults;
 
     const [dataLength, setDataLength] = useState("0");
+
+    // TODO: Move offset to context
+    const [offSet, setOffSet] = useState(10);
+
     const searchTerm = appContext.search.term;
 
-    const ResultsData = props => {
-        const { error, loading, data } = useQuery(CATALOGUE_ITEMS_SEARCH, {
-            variables: { searchTerm: props.searchTerm }
-        });
-
-        if (loading) return <CenterLoading active={true} withOverlay={false} description="Active loading indicator" />;
-        if (error) return <div>{error.status} - Error :(</div>;
-
-        setDataLength(data.hdrCatalogueItemsSearch.count);
-
-        const processedData = data && data.hdrCatalogueItemsSearch && data.hdrCatalogueItemsSearch.data;
-        return processedData && processedData.length > 0 ? (
-            processedData.map((result, i) => (
-                <ResultCard key={`resultCard-${i}`} title={result.label} description={result.description} />
-            ))
-        ) : (
-            <div>No results</div>
-        );
+    const onSearch = e => {
+        returnSearchResults(e);
+        setOffSet(10);
     };
 
     return (
         <div>
             <SearchHeading invisible={pageState}>{text.searchTitle}</SearchHeading>
             <SearchBarWrapper main={!pageState}>
-                <SearchBar labelText="Search" onKeyPress={returnSearchResults} />
+                <SearchBar labelText="Search" onKeyPress={onSearch} />
             </SearchBarWrapper>
             <Results invisible={!pageState}>
                 <SearchInfo>
@@ -68,12 +130,20 @@ const SearchPage = () => {
                         <Sort />
                     </SortDiv>
                 </SearchInfo>
-                <ResultsWrapper>
-                    <ResultsData searchTerm={searchTerm} />
-                </ResultsWrapper>
+                <ResultsData
+                    searchTerm={searchTerm}
+                    setDataLength={setDataLength}
+                    offSet={offSet}
+                    setOffSet={setOffSet}
+                    dataLength={dataLength}
+                />
             </Results>
         </div>
     );
 };
 
 export default SearchPage;
+
+// const filterObject = {
+
+// }
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,8 +1,8 @@
 import { gql } from "apollo-boost";
 
 export const CATALOGUE_ITEMS_SEARCH = gql`
-    query HdrCatalogueItemsSearch($searchTerm: String!) {
-        hdrCatalogueItemsSearch(recordLimit: 10, recordOffset: 0, searchTerm: $searchTerm) {
+    query HdrCatalogueItemsSearch($recordLimit: Int!, $recordOffset: Int!, $searchTerm: String!) {
+        hdrCatalogueItemsSearch(recordLimit: $recordLimit, recordOffset: $recordOffset, searchTerm: $searchTerm) {
             status
             message
             count
