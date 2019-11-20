0f9bf43fd258d8971a85a8590d75f643a38f61b4
trevorrwarduk <trevorrwarduk@gmail.com>
2019-11-20 15:45:46 +0000

    Removed commented code

--- src/HOC/AppContext/AppContext.js
+++ src/HOC/AppContext/AppContext.js
@@ -222,23 +222,6 @@ const AppContextProvider = props => {
             .catch(err => {
                 console.log("ERROR ", err);
             });
-        // .then(
-        //     result => {
-        //         this.setState({
-        //             isLoaded: true,
-        //             items: result.items
-        //         });
-        //     },
-        //     // Note: it's important to handle errors here
-        //     // instead of a catch() block so that we don't swallow
-        //     // exceptions from actual bugs in components.
-        //     error => {
-        //         this.setState({
-        //             isLoaded: true,
-        //             error
-        //         });
-        //     }
-        // );
     };
     return (
         <AppContext.Provider
