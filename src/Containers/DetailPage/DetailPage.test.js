cad821e36b4d8c8d2bc76bc268cd028b4e9deb14
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-11-25 12:02:41 +0000

    Updated tests

--- src/Containers/DetailPage/DetailPage.test.js
+++ src/Containers/DetailPage/DetailPage.test.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import { create, act } from "react-test-renderer";
 import DetailPage from "./DetailPage.js";
+
+import AppContextProvider, { AppContext } from "../../HOC/AppContext/AppContext.js";
+import apolloMock from "../../__mocks__/ApolloMock.js";
+
+import context from "../../__mocks__/AppContextMock.js";
+
+import { MemoryRouter } from "react-router-dom";
+import { MockedProvider } from "@apollo/react-testing";
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe("<DetailPage> ", () => {
     let renderedComponent;
 
     beforeEach(() => {
-        renderedComponent = create(<DetailPage />);
+        act(() => {
+            renderedComponent = create(
+                <MockedProvider mocks={apolloMock} addTypename={false}>
+                    <AppContextProvider value={context}>
+                        <MemoryRouter>
+                            <DetailPage />{" "}
+                        </MemoryRouter>{" "}
+                    </AppContextProvider>
+                </MockedProvider>
+            );
+        });
     });
 
-    it("should test the AboutPage Snapshot", () => {});
+    it("should render the detailed summary page with results", async () => {
+        await act(async () => await wait(1000));
+    });
 });
