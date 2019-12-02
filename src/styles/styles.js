6ec5d3c5ff642ec778113a552b92a651f3d9e34c
Tim Howard <tim.howard@ibm.com>
2019-12-02 15:26:40 +0000

    Undo styling fix due to other problems

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -195,7 +195,7 @@ export const ContentWrapper = styled.div`
     display: inline-block;
     width: ${p => (p.nav ? "calc(100% - 16rem)" : "100%")};
     left: ${p => (p.nav ? "16rem" : "0")};
-    height: calc(100vh - 17rem);
+    height: calc(100% - 17rem);
     min-height: 40rem;
     vertical-align: top;
 `;
