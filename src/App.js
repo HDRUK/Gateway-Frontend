86938d6714d41959212710aca36411030549d9de
trevorrwarduk <trevorrwarduk@gmail.com>
2019-12-05 11:57:33 +0000

    Initial work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Apollo from "./HOC/Apollo/Apollo.js";
 import AppContext from "./HOC/AppContext/AppContext.js";
 
+import ErrorBoundary from "./HOC/ErrorBoundary/ErrorBoundary";
+
 import LandingPage from "./Containers/LandingPage/LandingPage.js";
 import AboutPage from "./Containers/AboutPage/AboutPage.js";
 import SearchPage from "./Containers/SearchPage/SearchPage.js";
@@ -21,58 +23,60 @@ import DetailPage from "./Containers/DetailPage/DetailPage.js";
 class App extends React.Component {
     render() {
         return (
-            <Apollo>
-                <AppContext>
-                    <AppWrapper>
-                        <BrowserRouter>
-                            <GlobalStyle />
+            <ErrorBoundary>
+                <Apollo>
+                    <AppContext>
+                        <AppWrapper>
+                            <BrowserRouter>
+                                <GlobalStyle />
 
-                            <Header />
-                            <PageWrapper>
-                                <Switch>
-                                    <Route exact path="/">
-                                        <Content>
-                                            <LandingPage />
-                                        </Content>
-                                    </Route>
-                                    <Route path="/search">
-                                        <Content nav filter>
-                                            <MenuFilterWrapper />
-                                            <SearchPage />
-                                        </Content>
-                                    </Route>
-                                    <Route path="/my-searches">
-                                        <Content nav>
-                                            <MySearchesPage />
-                                        </Content>
-                                    </Route>
-                                    <Route path="/browse">
-                                        <Content nav>
-                                            <div>/browse</div>
-                                        </Content>
-                                    </Route>
-                                    <Route path="/about">
-                                        <Content nav>
-                                            <AboutPage />
-                                        </Content>
-                                    </Route>
-                                    <Route path="/help">
-                                        <Content nav>
-                                            <div>/help</div>
-                                        </Content>
-                                    </Route>
-                                    <Route path="/detail/:id">
-                                        <Content nav>
-                                            <DetailPage />
-                                        </Content>
-                                    </Route>
-                                </Switch>
-                            </PageWrapper>
-                            <Footer />
-                        </BrowserRouter>
-                    </AppWrapper>
-                </AppContext>
-            </Apollo>
+                                <Header />
+                                <PageWrapper>
+                                    <Switch>
+                                        <Route exact path="/">
+                                            <Content>
+                                                <LandingPage />
+                                            </Content>
+                                        </Route>
+                                        <Route path="/search">
+                                            <Content nav filter>
+                                                <MenuFilterWrapper />
+                                                <SearchPage />
+                                            </Content>
+                                        </Route>
+                                        <Route path="/my-searches">
+                                            <Content nav>
+                                                <MySearchesPage />
+                                            </Content>
+                                        </Route>
+                                        <Route path="/browse">
+                                            <Content nav>
+                                                <div>/browse</div>
+                                            </Content>
+                                        </Route>
+                                        <Route path="/about">
+                                            <Content nav>
+                                                <AboutPage />
+                                            </Content>
+                                        </Route>
+                                        <Route path="/help">
+                                            <Content nav>
+                                                <div>/help</div>
+                                            </Content>
+                                        </Route>
+                                        <Route path="/detail/:id">
+                                            <Content nav>
+                                                <DetailPage />
+                                            </Content>
+                                        </Route>
+                                    </Switch>
+                                </PageWrapper>
+                                <Footer />
+                            </BrowserRouter>
+                        </AppWrapper>
+                    </AppContext>
+                </Apollo>
+            </ErrorBoundary>
         );
     }
 }
