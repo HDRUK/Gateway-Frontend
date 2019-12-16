d07c18e87357a486478154f83a947a125cd86978
Tim Howard <tim.howard@ibm.com>
2019-12-16 16:58:10 +0000

    Fix to height of saved searches

--- src/Containers/MySearchesPage/styles.js
+++ src/Containers/MySearchesPage/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { ResultsWrapper } from "../../styles/styles.js";
 
 export const SavedSearchesWrapper = styled(ResultsWrapper)`
+    min-height: 36rem;
     height: ${p => (p.visible ? "calc(100vh - 8rem)" : "calc(100vh - 21rem)")};
     ${p => p.visible && "max-height: calc(100vh - 8rem);"};
 `;
