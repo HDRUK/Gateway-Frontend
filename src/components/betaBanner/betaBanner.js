6624c3271322c678bc132cae3f6f9301bb85bc75
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2020-01-06 10:31:07 +0000

    Update links

--- src/components/betaBanner/betaBanner.js
+++ src/components/betaBanner/betaBanner.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { BetaWrapper, ParagraphText, BetaLabel, SmallSpace, TinySpace } from "../../styles/styles.js";
 
-const textItem = "This is a new service - your feedback will help us improve it.";
+const textItems = {
+    firstText: "This is a new service - your ",
+    linkText: "feedback",
+    secondText: " will help us improve it."
+};
+
+const link = "https://docs.google.com/forms/d/e/1FAIpQLSdPuRFdqG7f0PA3OfJORxax9Mgk7I0H3c4ocHPlGFvuCExdhw/viewform";
 
 const BetaBanner = () => (
     <BetaWrapper>
         <TinySpace></TinySpace>
         <BetaLabel>BETA</BetaLabel>
         <SmallSpace></SmallSpace>
-        <ParagraphText>{textItem}</ParagraphText>
+        <ParagraphText>
+            {textItems.firstText}
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                {textItems.linkText}
+            </a>
+            {textItems.secondText}
+        </ParagraphText>
     </BetaWrapper>
 );
 
